da65ac2c8bfa2d5df70fbb4491cd73690b82e56e
NJLIGAMES <jamesfolk1@gmail.com>
2016-06-24 03:06:17 +0000

    added a json response demo

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 from flask import Flask
+from flask import Response
+from flask import json
+
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+#@app.route('/')
+#def hello():
+    #return "Hello World!"
 
-@app.route('/<name>')
-def hello_name(name):
-	return "Hello {}!".format(name)
+#@app.route('/<name>')
+#def hello_name(name):
+	#return "Hello {}!".format(name)
+
+@app.route('/hello', methods = ['GET'])
+def api_hello():
+	data = {'hello'  : 'world','number' : 3}
+	js = json.dumps(data)
+
+	resp = Response(js, status=200, mimetype='application/json')
+	resp.headers['Link'] = 'http://luisrei.com'
+
+	return resp
 
 #http://10.0.1.17:5000
 if __name__ == '__main__':
